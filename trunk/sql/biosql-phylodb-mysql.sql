d90c39c9d71f22c037b9c487aa0f01bf000f467c
JamesEstill <JamesEstill@ae3e89b5-dc2e-0410-93e6-c7291bf5f99e>
2007-06-04 03:40:01 +0000

    Added SQL dir Added biosql-phylodb-mysql-sql

---
+++ trunk/sql/biosql-phylodb-mysql.sql
@@ -0,0 +1,125 @@
+-- $Id: biosql-phylodb-pg.sql,v 1.1 2007/03/12 17:36:19 lapp Exp $
+
+-- Schema extension on top of the BioSQL core schema for representing
+-- phylogenetic trees or networks (anastomizing and reticulating).
+--
+-- This was developed independently but is very similar to the
+-- phylogeny module in Chado (the GMOD common relational model).
+
+-- Authors: Hilmar Lapp, Bill Piel
+--
+-- (c) Hilmar Lapp, hlapp at gmx.net, 2007
+-- (c) Bill Piel, william.piel at yale.edu, 2007. 
+-- You may use, modify, and distribute this code under the same terms as Perl.
+-- See the Perl Artistic License.
+--
+-- comments to biosql - biosql-l@open-bio.org
+
+-- Jamie made a few changes to make htis work in MySQL these may not
+-- be correct, but it loaded in MySQL without errors 
+
+-- the tree - conceptually equal to a namespace (a way to scope nodes and edges)
+CREATE TABLE tree (
+       tree_id INTEGER NOT NULL auto_increment,
+       name VARCHAR(32) NOT NULL,
+       identifier VARCHAR(16),
+       node_id INTEGER NOT NULL -- startpoint of tree
+       , PRIMARY KEY (tree_id)
+       , UNIQUE (name)
+);
+
+-- nodes in a tree
+CREATE TABLE node (
+       node_id INTEGER NOT NULL auto_increment,
+       label VARCHAR(255),
+       tree_id INTEGER NOT NULL,
+       gene_id INTEGER,
+       taxon_id INTEGER,
+       left_idx INTEGER,
+       right_idx INTEGER
+       , PRIMARY KEY (node_id)
+       , UNIQUE (label,tree_id)
+       , UNIQUE (left_idx,tree_id)
+       , UNIQUE (right_idx,tree_id)
+);
+
+-- edges between nodes
+CREATE TABLE edge (
+       edge_id INTEGER NOT NULL auto_increment,
+       child_node_id INTEGER NOT NULL,
+       parent_node_id INTEGER NOT NULL
+       , PRIMARY KEY (edge_id)
+       , UNIQUE (child_node_id,parent_node_id)
+);       
+
+-- transitive closure over edges between nodes
+CREATE TABLE node_path (
+       child_node_id INTEGER NOT NULL,
+       parent_node_id INTEGER NOT NULL,
+       path TEXT,
+       distance INTEGER
+       , PRIMARY KEY (child_node_id,parent_node_id,distance)
+);       
+
+-- attribute/value pairs for edges
+CREATE TABLE edge_attribute_value (
+       value text,
+       edge_id INTEGER NOT NULL,
+       term_id INTEGER NOT NULL
+       , UNIQUE (edge_id,term_id)
+);
+
+-- attribute/value pairs for nodes
+CREATE TABLE node_attribute_value (
+       value text,
+       node_id INTEGER NOT NULL,
+       term_id INTEGER NOT NULL
+       , UNIQUE (node_id,term_id)
+);
+
+-- The pg below
+--ALTER TABLE tree ADD CONSTRAINT FKnode
+--       FOREIGN KEY (node_id) REFERENCES node (node_id)
+--           DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE tree ADD CONSTRAINT FKnode
+       FOREIGN KEY (node_id) REFERENCES node (node_id);
+
+ALTER TABLE node ADD CONSTRAINT FKnode_tree
+       FOREIGN KEY (tree_id) REFERENCES tree (tree_id);
+
+ALTER TABLE node ADD CONSTRAINT FKnode_bioentry
+       FOREIGN KEY (gene_id) REFERENCES bioentry (bioentry_id);
+
+ALTER TABLE node ADD CONSTRAINT FKnode_taxon
+       FOREIGN KEY (taxon_id) REFERENCES taxon (taxon_id);
+
+ALTER TABLE edge ADD CONSTRAINT FKedge_child
+       FOREIGN KEY (child_node_id) REFERENCES node (node_id)
+           ON DELETE CASCADE;
+
+ALTER TABLE edge ADD CONSTRAINT FKedge_parent
+       FOREIGN KEY (parent_node_id) REFERENCES node (node_id)
+           ON DELETE CASCADE;
+
+ALTER TABLE node_path ADD CONSTRAINT FKnpath_child
+       FOREIGN KEY (child_node_id) REFERENCES node (node_id)
+           ON DELETE CASCADE;
+
+ALTER TABLE node_path ADD CONSTRAINT FKnpath_parent
+       FOREIGN KEY (parent_node_id) REFERENCES node (node_id)
+           ON DELETE CASCADE;
+
+ALTER TABLE edge_attribute_value ADD CONSTRAINT FKeav_edge
+       FOREIGN KEY (edge_id) REFERENCES edge (edge_id)
+           ON DELETE CASCADE;
+
+ALTER TABLE edge_attribute_value ADD CONSTRAINT FKeav_term
+       FOREIGN KEY (term_id) REFERENCES term (term_id);
+
+ALTER TABLE node_attribute_value ADD CONSTRAINT FKnav_node
+       FOREIGN KEY (node_id) REFERENCES node (node_id)
+           ON DELETE CASCADE;
+
+ALTER TABLE node_attribute_value ADD CONSTRAINT FKnav_term
+       FOREIGN KEY (term_id) REFERENCES term (term_id);
