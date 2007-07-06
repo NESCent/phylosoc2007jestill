fff72d91fc0c8d1ecf6f27760ebfc390fb2b3bae
JamesEstill <JamesEstill@ae3e89b5-dc2e-0410-93e6-c7291bf5f99e>
2007-07-06 18:58:28 +0000

    Added to SQL code in phymodwork.sql

--- trunk/scripts/phymodwork.sql
+++ trunk/scripts/phymodwork.sql
@@ -13,10 +13,6 @@
 -- given a parent node
 -- This makes use of the parent node information
 -- from the node_path table
-
-
--- This also returns the root node for
--- the selected parent node
 --SELECT e.edge_id, 
 --       pt.node_id AS parent_node_id, 
 --       pt.label AS parent_label, 
@@ -36,8 +32,21 @@
 -- The above can be modified for delete queries
 -- Using a nested SQL query
 
+
+
+
+
+-------------------------------------------------------------+
+-- THE FOLLOWING CODE IS RELATED TO NEEDS OF THE             |
+-- CUT AND COPY QUERIES                                      |
+-------------------------------------------------------------+
+
+system echo '==============================';
+system echo ' BASE SELECT QUERY FOR NODES';
+system echo '==============================';
+
 -- This is the select query which will
--- be used below
+-- be used below as part of a nested query
 SELECT pt.node_id AS del_node_id 
 FROM node_path p, edge e, node pt, node ch 
 WHERE e.child_node_id = p.child_node_id
@@ -45,18 +54,27 @@ AND pt.node_id = e.parent_node_id
 AND ch.node_id = e.child_node_id
 AND p.parent_node_id = '25';
 
-
-
--- A simple query
---SELECT * FROM node_path
---WHERE parent_node_id = '10';
-
-SELECT * FROM node_path WHERE parent_node_id = '10';
-
+-------------------------------+
+-- TABLE: node_path           
+-------------------------------+
+system echo '==============================';
+system echo ' TABLE: node_path';
+system echo '==============================';
 -- THESE ARE RECORDS FROM node_path WHERE 
 -- the children of the parent_node are
 -- parents
 SELECT * FROM node_path 
+WHERE parent_node_id IN
+(
+ SELECT pt.node_id 
+ FROM node_path p, edge e, node pt, node ch 
+ WHERE e.child_node_id = p.child_node_id
+ AND pt.node_id = e.parent_node_id
+ AND ch.node_id = e.child_node_id
+ AND p.parent_node_id = '25'
+);
+
+SELECT COUNT(*) FROM node_path 
 WHERE parent_node_id IN
 (
  SELECT pt.node_id 
@@ -81,4 +99,135 @@ WHERE child_node_id IN
  AND p.parent_node_id = '25'
 );
 
---Attributes
+SELECT COUNT(*) FROM node_path 
+WHERE child_node_id IN
+(
+ SELECT pt.node_id 
+ FROM node_path p, edge e, node pt, node ch 
+ WHERE e.child_node_id = p.child_node_id
+ AND pt.node_id = e.parent_node_id
+ AND ch.node_id = e.child_node_id
+ AND p.parent_node_id = '25'
+);
+
+-------------------------------+
+-- TABLE: node_attribute_value |
+-------------------------------+
+system echo '==============================';
+system echo ' TABLE: node_attribute_value';
+system echo '==============================';
+SELECT * FROM node_attribute_value 
+WHERE node_id IN
+(
+ SELECT pt.node_id 
+ FROM node_path p, edge e, node pt, node ch 
+ WHERE e.child_node_id = p.child_node_id
+ AND pt.node_id = e.parent_node_id
+ AND ch.node_id = e.child_node_id
+ AND p.parent_node_id = '25'
+);
+
+SELECT COUNT(*) FROM node_attribute_value 
+WHERE node_id IN
+(
+ SELECT pt.node_id 
+ FROM node_path p, edge e, node pt, node ch 
+ WHERE e.child_node_id = p.child_node_id
+ AND pt.node_id = e.parent_node_id
+ AND ch.node_id = e.child_node_id
+ AND p.parent_node_id = '25'
+);
+
+
+-------------------------------+
+-- TABLE: node                 |
+-------------------------------+
+system echo '==============================';
+system echo ' TABLE: node';
+system echo '==============================';
+SELECT * FROM node 
+WHERE node_id IN
+(
+ SELECT pt.node_id 
+ FROM node_path p, edge e, node pt, node ch 
+ WHERE e.child_node_id = p.child_node_id
+ AND pt.node_id = e.parent_node_id
+ AND ch.node_id = e.child_node_id
+ AND p.parent_node_id = '25'
+);
+
+
+SELECT COUNT(*) FROM node 
+WHERE node_id IN
+(
+ SELECT pt.node_id 
+ FROM node_path p, edge e, node pt, node ch 
+ WHERE e.child_node_id = p.child_node_id
+ AND pt.node_id = e.parent_node_id
+ AND ch.node_id = e.child_node_id
+ AND p.parent_node_id = '25'
+);
+
+-------------------------------+
+-- TABLE: edge_attribute_value |
+-------------------------------+
+--SELECT * FROM node_attribute_value 
+--WHERE node_id IN
+--(
+-- SELECT pt.node_id 
+-- FROM node_path p, edge e, node pt, node ch 
+-- WHERE e.child_node_id = p.child_node_id
+-- AND pt.node_id = e.parent_node_id
+-- AND ch.node_id = e.child_node_id
+-- AND p.parent_node_id = '25'
+--);
+
+
+-------------------------------+
+-- TABLE: edge
+-------------------------------+
+system echo '==============================';
+system echo ' TABLE: edge';
+system echo '==============================';
+SELECT e.edge_id 
+FROM node_path p, 
+     edge e, 
+     node pt, 
+     node ch 
+WHERE 
+    e.child_node_id = p.child_node_id
+AND pt.node_id = e.parent_node_id
+AND ch.node_id = e.child_node_id
+AND p.parent_node_id = '25';
+
+
+SELECT * from edge 
+WHERE edge_id IN
+(
+ SELECT e.edge_id 
+ FROM node_path p, edge e, node pt, node ch 
+ WHERE e.child_node_id = p.child_node_id
+ AND pt.node_id = e.parent_node_id
+ AND ch.node_id = e.child_node_id
+ AND p.parent_node_id = '25'
+);
+
+
+
+
+-------------------------------+
+-- TABLE: edge_attribute_value |
+-------------------------------+
+system echo '==================================';
+system echo ' TABLE: edge_attribute_value info';
+system echo '==================================';
+SELECT * FROM edge_attribute_value
+WHERE edge_id IN 
+(
+ SELECT e.edge_id 
+ FROM node_path p, edge e, node pt, node ch 
+ WHERE e.child_node_id = p.child_node_id
+ AND pt.node_id = e.parent_node_id
+ AND ch.node_id = e.child_node_id
+ AND p.parent_node_id = '25'
+);
